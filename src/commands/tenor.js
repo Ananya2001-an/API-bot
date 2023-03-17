fe6f106b307520dd23f120ae858338442e3078b1
Ananya Nayak <ananyanayak102@gmail.com>
2023-03-17 06:05:02 +0000

    feat: implemented tenor api command

---
+++ src/commands/tenor.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require('axios');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("tenor-gif")
+    .setDescription("Fetches a Tenor gif!")
+    .addStringOption((option) =>
+      option
+        .setName("access_key")
+        .setDescription("Your Tenor Access Key")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("query")
+        .setDescription("Give search query for Tenor gif")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const key = interaction.options.getString('access_key');
+    const query = interaction.options.getString("query");
+
+    const res = await axios.get(
+      `https://tenor.googleapis.com/v2/search?q=${query}&key=${key}`
+    );
+    await interaction.reply(res.data.results[0].media_formats.gif.url);
+  },
+};
